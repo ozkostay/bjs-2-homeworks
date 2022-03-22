1ef2aee8652341d0d410a307e5cfcc7bd477db9e
Konstantin Ozhug <ozkostay@mail.ru>
2022-03-22 19:54:01 +0500

    first task

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,15 @@
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
+  let arr = [];
+  let discriminant;
+  discriminant = b**2 - 4*a*c;
+  if (discriminant > 0) {
+    arr[0] = (-b + Math.sqrt(discriminant)) / (2*a);
+    arr[1] = (-b - Math.sqrt(discriminant)) / (2*a);
+  } else if (discriminant === 0) {
+    arr[0] = -b / (2*a);
+  }
   return arr; // array
 }
 
