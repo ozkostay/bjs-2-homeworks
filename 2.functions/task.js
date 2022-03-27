1f6b07d8c0c746a28765759144196f3f81192049
Konstantin Ozhug <ozkostay@mail.ru>
2022-03-27 21:02:11 +0500

    2.functions-1

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,9 +1,17 @@
 // Задание 1
 function getArrayParams(arr) {
   let min, max, sum, avg;
-
-  // Ваш код
-
+  min = arr[0];
+  max = arr[0];
+  avg = 0;
+  for (let i = 0; i < arr.length ; i++) {
+    if (arr[i] < min) {
+      min = arr[i];  
+    }
+    max = arr[i] > max ? arr[i] : max;
+    avg = avg+arr[i];  
+  }
+  avg = Number((avg/arr.length).toFixed(2));
   return { min: min, max: max, avg: avg };
 }
 
