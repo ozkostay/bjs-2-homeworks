1b05c880f1bcd0ab725eaf7a30cf1989c070fe5e
ozkostay <ozkostay@mail.ru>
2022-04-11 18:17:59 +0500

    4.Object start

--- 4.objects/main.js
+++ 4.objects/main.js
@@ -3,13 +3,14 @@ student1.setSubject("Algebra");
 student1.addMark(5);
 student1.addMark(4);
 student1.addMark(5);
+student1.addMarks(3,3,4);
 
 let student2 = new Student("Buzz", "female", 35);
 student2.setSubject("Geometry");
 student2.addMark(2);
 student2.addMark(3);
 student2.addMark(2);
-student2.exclude('low grades')
+student2.exclude('low grades');
 
-console.log(student1); 
+console.log(student1);
 console.log(student2);
--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,34 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) {
+    this.marks = [mark];
+  } else {
+    this.marks.push(mark);
+  }
+}
+
+Student.prototype.addMarks = function (...mark) {
+  if (this.marks === undefined) {
+    this.marks = [];
+  } else {
+    mark.every((nextmark,index) => this.marks.push(mark[index]));
+    
+    //this.marks.push(mark);
+  }
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this.marks;
+  delete this.subject;
+  this.excluded = reason;
+}
 // ваш код для остальных методов
