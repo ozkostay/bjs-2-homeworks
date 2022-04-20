0674e4d46fc55721e0481f8427111bcb22338058
ozkostay <ozkostay@mail.ru>
2022-04-20 15:27:36 +0500

    6.exception-closure -1

---
+++ 6.exception-closure/main.js
@@ -0,0 +1,14 @@
+// тут вы можете вызывать функции из task.js
+//console.log(parseCount("x10px"));
+console.log(validateCount("45ыфва"));
+
+console.log(111);
+const aaa = new Triangle (11,29,33);
+console.log(aaa);
+console.log(aaa.getPerimeter());
+console.log(aaa.getArea());
+
+console.log("============= GetTriangle 1");
+console.log(getTriangle(2,5,5));
+console.log("============= GetTriangle 1");
+console.log(getTriangle(1,3,100));
---
+++ 6.exception-closure/task.js
@@ -0,0 +1,52 @@
+//================= Задача 1 ===================
+function parseCount(value) {
+    let persresult = parseInt(value);
+    if (!persresult) throw new Error("Невалидное значение");
+    return persresult;
+}
+
+function validateCount(value) {
+    let aaa;
+    try {
+        return parseCount(value); 
+    }
+    catch (err){
+        return err;
+    }
+}
+
+//================= Задача 2 ===================
+
+class Triangle {
+    constructor(sideA,sideB,sideC) {
+        if ((sideA + sideB) <= sideC || (sideA + sideC) <= sideB || (sideB + sideC) <= sideA){
+            console.log("ошибка");
+            throw new Error("Треугольник с такими сторонами не существует");
+        } else {
+            console.log("норм");
+        }
+        this.sideA = sideA;
+        this.sideB = sideB;
+        this.sideC = sideC;
+    }
+    getPerimeter() {
+        return this.sideA + this.sideB + this.sideC;
+    }
+    getArea() {
+        let halfPerimeter = (this.sideA + this.sideB + this.sideC) * 0.5;
+        return Number((Math.sqrt(halfPerimeter*(halfPerimeter - this.sideA)*(halfPerimeter - this.sideB)*(halfPerimeter - this.sideC))).toFixed(3));
+    }
+}
+
+function getTriangle(sideA,sideB,sideC) {
+    try {
+        return new Triangle(sideA,sideB,sideC);
+    }
+    catch {
+        const objForRiturn= {
+            getPerimeter: () => "Ошибка! Треугольник не существует",
+            getArea: () => "Ошибка! Треугольник не существует",
+        }
+        return objForRiturn;
+    }
+}
