0bf9d2f17efbb9e5f2531644176b99786411a2d7
Konstantin Ozhug <ozkostay@mail.ru>
2022-05-23 12:12:52 +0500

    8.decor - 2 230522

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -10,23 +10,17 @@ function cachingDecoratorNew(func) {
     if (idx !== -1 ) {
       console.log('Из кэша: ',cache[idx].value);
       return 'Из кэша: ' + cache[idx].value;
-    } else {
-      if (cache.length >= 5) {
-        cache.shift();
-        console.log('Удаляем старый элемент');
-      }
-      
-      let result = func(...args);
-      sumArray = 0;
-      
-      for (let i = 0; i < args.length; i++) {
-        sumArray += args[i];
-      }
-      
-      cache.push({hash: hash, value: sumArray });
-      console.log('Вычисляем: ',result);
-      return 'Вычисляем: ' + result;
     }
+
+    if (cache.length >= 5) {
+      cache.shift();
+      console.log('Удаляем старый элемент');
+    }
+    
+    let result = func(...args);
+    cache.push({hash: hash, value: result});
+    console.log('Вычисляем: ',result);
+    return 'Вычисляем: ' + result;
   }
 
   return wrapper;
@@ -36,8 +30,12 @@ function cachingDecoratorNew(func) {
 function debounceDecoratorNew(func, ms) {
   // Ваш код
   let timeout;
-
+  
   return function (...args) {
+    if (!timeout) {
+      func();
+    }
+
     clearTimeout(timeout);
     timeout = setTimeout( () => {
       func.apply(this, args);
@@ -48,8 +46,13 @@ function debounceDecoratorNew(func, ms) {
 function debounceDecorator2(func, ms) {
   // Ваш код
   let timeout;
-  //let counts;
+  
   function wrapper(...args) {
+    if (!timeout) {
+      func();
+      wrapper.counts += 1;
+    }
+
     wrapper.counts += 1;
     clearTimeout(timeout);
     timeout = setTimeout( () => {
